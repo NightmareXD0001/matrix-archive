a781e7001eb973ea822ab6aa79c6798f1436c2ea
Nightmare <137179116+NightmareXD0001@users.noreply.github.com>
2025-05-17 00:42:56 +0530

    Update batchDataPsynapse.ts

--- src/data/batchDataPsynapse.ts
+++ src/data/batchDataPsynapse.ts
@@ -5,52 +5,19 @@ export const batchDataPsynapse: BatchData[] = [
   {
     year: "2024-2025",
     members: [
-      {
-        position: "Lead Psychologist",
-        names: ["Ishaan Bhandari"]
-      },
-      {
-        position: "Clinical Director",
-        names: ["Aditya Kumar Sharma"]
-      },
-      {
-        position: "Research Head",
-        names: ["HariKarthik V. K."]
-      },
-      {
-        position: "Therapy Coordinator",
-        names: ["Aarush Sood"]
-      },
-      {
-        position: "Research Associates",
-        names: ["Manan Trivedi", "Samyak Singh", "Ananya Gupta", "Rohan Verma", "Priya Sharma", "Dev Patel"]
-      }
+      { position: "President", names: ["Daksh"] },
+      { position: "Vice President", names: ["Dhriti Yadav"] },
+      { position: "General Secretary", names: ["Roger Sebastian"] },
+      { position: "Joint Secretary", names: ["Chinghoih"] }
     ]
   },
   {
     year: "2023-2024",
     members: [
-      { position: "Lead Psychologist", names: ["Shashwat Upadhay"] },
-      { position: "Clinical Director", names: ["Anish Kapila"] },
-      { position: "Research Head", names: ["Ishan Khanna"] },
-      { position: "Therapy Coordinator", names: ["Khushhal Sharma"] },
-      {
-        position: "Research Associates",
-        names: ["Samyak Singh", "Manan Trivedi", "Chinmayee Khanna", "Arham Jain", "Aarav Tiwari", "Allen Wilson"]
-      }
-    ]
-  },
-  {
-    year: "2022-2023",
-    members: [
-      { position: "Lead Psychologist", names: ["Palash Gupta"] },
-      { position: "Clinical Director", names: ["Prakhar Alok Semwal"] },
-      { position: "Research Head", names: ["Hans James"] },
-      { position: "Therapy Coordinator", names: ["Rishabh Aggarwal"] },
-      {
-        position: "Research Associates",
-        names: ["Abhigyan Singh Negi", "Ajay Dhalla", "Bhavay Yadav", "Lakshya Gupta", "Pious Babu", "Poorva Dwivedi", "Samarth Arora"]
-      }
+      { position: "President", names: ["Tavleen Kaur"] },
+      { position: "Vice President", names: ["Daksha Bisht"] },
+      { position: "General Secretary", names: ["Jyotsana Chaudhary"] },
+      { position: "Joint Secretary", names: ["Daksh"] }
     ]
   }
 ];

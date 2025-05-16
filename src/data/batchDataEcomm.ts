6048736cae0d98375649cd0ddf39355d36780b23
Nightmare <137179116+NightmareXD0001@users.noreply.github.com>
2025-05-17 00:22:23 +0530

    Update batchDataEcomm.ts

--- src/data/batchDataEcomm.ts
+++ src/data/batchDataEcomm.ts
@@ -1,56 +1,133 @@
-
 import { BatchData } from "@/components/BatchCard";
 
 export const batchDataEcomm: BatchData[] = [
   {
-    year: "2024-2025",
-    members: [
-      {
-        position: "Chief Executive Officer",
-        names: ["Ishaan Bhandari"]
-      },
-      {
-        position: "Chief Operating Officer",
-        names: ["Aditya Kumar Sharma"]
-      },
-      {
-        position: "Chief Financial Officer",
-        names: ["HariKarthik V. K."]
-      },
-      {
-        position: "Marketing Director",
-        names: ["Aarush Sood"]
-      },
-      {
-        position: "Business Analysts",
-        names: ["Manan Trivedi", "Samyak Singh", "Ananya Gupta", "Rohan Verma", "Priya Sharma", "Dev Patel"]
-      }
-    ]
-  },
-  {
     year: "2023-2024",
     members: [
-      { position: "Chief Executive Officer", names: ["Shashwat Upadhay"] },
-      { position: "Chief Operating Officer", names: ["Anish Kapila"] },
-      { position: "Chief Financial Officer", names: ["Ishan Khanna"] },
-      { position: "Marketing Director", names: ["Khushhal Sharma"] },
-      {
-        position: "Business Analysts",
-        names: ["Samyak Singh", "Manan Trivedi", "Chinmayee Khanna", "Arham Jain", "Aarav Tiwari", "Allen Wilson"]
-      }
+      { position: "President", names: ["Shubaan Manuja"] },
+      { position: "Vice President", names: ["Parham Kanth"] },
+      { position: "General Secretary", names: ["Leesha Dwivedi"] },
+      { position: "Joint Secretary", names: ["Jahnavi Verma"] },
+      { position: "Executive Member", names: ["Mahima Soam", "Avraaj Singh Anand", "Ikjot Singh Anand", "Kajsimar Kour", "Karunay Malik", "Prajnav S. Murthy", "Sakshi Kumari", "Vikrant Sethi"] }
     ]
   },
   {
     year: "2022-2023",
     members: [
-      { position: "Chief Executive Officer", names: ["Palash Gupta"] },
-      { position: "Chief Operating Officer", names: ["Prakhar Alok Semwal"] },
-      { position: "Chief Financial Officer", names: ["Hans James"] },
-      { position: "Marketing Director", names: ["Rishabh Aggarwal"] },
-      {
-        position: "Business Analysts",
-        names: ["Abhigyan Singh Negi", "Ajay Dhalla", "Bhavay Yadav", "Lakshya Gupta", "Pious Babu", "Poorva Dwivedi", "Samarth Arora"]
-      }
+      { position: "President", names: ["Arjun Sehrawat"] },
+      { position: "Vice President", names: ["Parth Jain"] },
+      { position: "General Secretary", names: ["Andrew Puttam"] },
+      { position: "Joint Secretary", names: ["Nikunj Hasteer"] },
+      { position: "Executive Member", names: ["Khyaati Bhadauria", "Jaiditya Hora", "Arnav Manchanda", "Rayna Adlakha", "Noor Dhingra", "Aquin Manuvel", "Ayushi Gupta", "Aditya Jacob Bijoy"] }
+    ]
+  },
+  {
+    year: "2021-2022",
+    members: [
+      { position: "President", names: ["Jonathan Felix D'Lima"] },
+      { position: "Vice President", names: ["Hansin Sadhu"] },
+      { position: "General Secretary", names: ["Gazal Singh"] },
+      { position: "Joint Secretary", names: ["Sneha Sharma"] },
+      { position: "Executive Member", names: ["Arohan Tewatia", "Diya Rajesh Gulia", "Pia Mehra", "Kyna Marwah", "Parth Jain", "Pradan Samuel", "Arjun Sehrawat", "Gurpriya Kaur"] }
+    ]
+  },
+  {
+    year: "2020-2021",
+    members: [
+      { position: "President", names: ["Kisha Jain"] },
+      { position: "Vice President", names: ["Raghav Kapoor"] },
+      { position: "General Secretary", names: ["Arjun Mandal"] },
+      { position: "Joint Secretary", names: ["Vanshika Rai"] },
+      { position: "Executive Member", names: ["Garima Tanwar", "Dishti Girotra", "Shimona Dandona", "Janvi Jain", "Shlok Nanda", "Reha Khajuria", "Gauri Rampal", "Jonathan D'Lima", "Pia Mehra", "Lavanya Dua"] }
+    ]
+  },
+  {
+    year: "2019-2020",
+    members: [
+      { position: "President", names: ["Sona Serene"] },
+      { position: "Vice President", names: ["Vaibhav Seth"] },
+      { position: "General Secretary", names: ["Anirudh Ghildiyal"] },
+      { position: "Joint Secretary", names: ["Ann Riya Rajeev"] },
+      { position: "Executive Member", names: ["Dhriti Lakhnpal", "Meher Saini", "Rijul Chadha", "Aarav Tanwar", "Alex Damien Ekka", "Tanisha Kataria", "Janhavi Narang", "Shreya Gupta", "Swastika Dahiya", "Vanshika Rai", "Arjun Mandal", "Kisha Jain"] }
+    ]
+  },
+  {
+    year: "2018-2019",
+    members: [
+      { position: "President", names: ["Eshaan Tandon"] },
+      { position: "Vice President", names: ["Kevin Jones"] },
+      { position: "General Secretary", names: ["Rochelle Prakash"] },
+      { position: "Joint Secretary", names: ["Teesha Seth"] },
+      { position: "Executive Member", names: ["Shikhar Singh", "Yajat Gulia", "Conchita Sahu", "Chhavi Garg", "Joshua Mario Xavier", "Rashmika Gupta", "Aditi Singh", "Kush Badola", "Vaibhav Seth", "Raymond Joseph", "Anirudh Ghildiyal", "Sona Serene"] }
+    ]
+  },
+  {
+    year: "2017-2018",
+    members: [
+      { position: "President", names: ["Animan Parashar"] },
+      { position: "Vice-President", names: ["Manushrie Verma"] },
+      { position: "General Secretary", names: ["Snigdha Sharma"] },
+      { position: "Joint Secretaries", names: ["Jiya Sharma"] },
+      { position: "Executive Member", names: ["Eshaan Tandon", "Rashmika Gupta", "Kevin Jones", "Shikhar Singh", "Conchita Sahu", "Teesha Seth", "Shivansh Sood", "Tanvi Anand", "Dikshant Tanwar", "Himmat Singh", "Abhimanyu Deswal", "George Toppo"] }
+    ]
+  },
+  {
+    year: "2016-2017",
+    members: [
+      { position: "President", names: ["Prableen Kaur Chandhok"] },
+      { position: "Vice-President", names: ["Ashwin Bhanot"] },
+      { position: "General Secretary", names: ["Kalyani Nair"] },
+      { position: "Joint Secretaries", names: ["Kartik Vinayak"] },
+      { position: "Executive Member", names: ["Utkarsh Kakkar", "Anni Bhardwaj", "Bhawesh Sahni", "Devansh Bhardwaj", "Dhruv Tiwari", "Sandeep Malhotra", "Snigdha Sharma", "Manushrie Verma", "Jiya Sharma", "Tanvi Anand", "Animan Parashar", "Himmat Singh"] }
+    ]
+  },
+  {
+    year: "2015-2016",
+    members: [
+      { position: "President", names: ["Chaitanya Chadha"] },
+      { position: "Vice-President", names: ["Shaurya Pokhriyal"] },
+      { position: "General Secretary", names: ["Jason Lobo"] },
+      { position: "Joint Secretaries", names: ["Paul Babu"] },
+      { position: "Executive Member", names: ["Apoorv Phillips", "Arjan Singh", "Ashley Wilkinson", "Navya Achantani", "Pranay Sundriyal", "Rishabh Chauhan", "Anni Bhardwaj", "Ashwin Bhanot", "Kalyani Nair", "Kartik Vinayak", "Prableen Kaur", "Vedika Dewan"] }
+    ]
+  },
+  {
+    year: "2013-2014",
+    members: [
+      { position: "President", names: ["Sharath Chandran"] },
+      { position: "Vice-President", names: ["Dhruv Dua"] },
+      { position: "General Secretary", names: ["Kshitij Dua"] },
+      { position: "Joint Secretaries", names: ["Karan Chauhan"] },
+      { position: "Executive Member", names: ["Chinmay Indurkar", "Shivanker Tandon", "Kartik Sood", "Sahib S Bawa", "Janesh Jain", "Pratinav Girsa", "Shouriya Sirohi", "Devanjal Singh", "Neil Jacob Oomen", "Parth Malik", "Shashank Sharma", "Deepansh Garg"] }
+    ]
+  },
+  {
+    year: "2012-2013",
+    members: [
+      { position: "President", names: ["Prateek Paul"] },
+      { position: "Vice-President", names: ["Amanbir Bajwa"] },
+      { position: "General Secretary", names: ["Anurag Jacob"] },
+      { position: "Joint Secretaries", names: ["Dominic Xavier"] },
+      { position: "Executive Member", names: ["Devesh Sethi", "Shrrey Mehta", "Jaskaran Singh Narang", "Kushagra Ahuja", "Jatin Bhagat", "Konish Naidu"] }
+    ]
+  },
+  {
+    year: "2011-2012",
+    members: [
+      { position: "President", names: ["Raag Bhasin"] },
+      { position: "Vice-President", names: ["Mark Pavarmani"] },
+      { position: "Secretary", names: ["Roy Mathew Joseph"] },
+      { position: "Executive Member", names: ["Hetish Singh", "Nitish Gupta", "Anurag Yadav", "Aniket Gawas"] }
+    ]
+  },
+  {
+    year: "2010-2011",
+    members: [
+      { position: "President", names: ["Ayushman Mohan"] },
+      { position: "Vice-President", names: ["Raag Bhasin"] },
+      { position: "Joint Secretaries", names: ["Manav Bhalla"] },
+      { position: "Executive Member (Class 12)", names: ["Aviral Gupta", "Ayush Khanijo", "Abhinav Sethi", "Anuj Jhokani", "Deepak Malyala", "Madhav Dhir Kohli", "Vikram Deep Singh Dayal"] },
+      { position: "Executive Member (Class 11)", names: ["Aniket Gawas", "Mark Pavamani", "Hetish Raj Singh", "Roy Joseph", "Manaved Nambiar", "Nitish Aggarwal"] }
     ]
   }
 ];
